7a7892c5ea6158b198e136c670a53e1683f1958c
Memoh <CraftMan3512@gmail.com>
2023-02-25 12:04:48 -0500

    setup for card holder code and scene game

--- Assets/scripts/JackPlayer.cs
+++ Assets/scripts/JackPlayer.cs
@@ -6,6 +6,7 @@ using UnityEngine.Events;
 public class JackPlayer : MonoBehaviour
 {
     public List<Card> hand;
+    public List<CardHolder> holders;
     public int money;
     
     protected UnityEvent OnCardAskComplete;
