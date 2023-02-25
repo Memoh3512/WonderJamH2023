e40d2443cc0448ed08c0ba15923342a7a3b623a2
Memoh <CraftMan3512@gmail.com>
2023-02-24 21:22:58 -0500

    distract all

--- Assets/scripts/BlackJackManager.cs
+++ Assets/scripts/BlackJackManager.cs
@@ -9,12 +9,18 @@ public class BlackJackManager
         yield return new WaitForSeconds(0);
     }
 
-    public static void Distract(JackPlayer player, float distractionValue)
+    public static void Distract(AIJackPlayer player, float distractionValue)
     {
         //distraction
+        player.distractionLevel = distractionValue;
     }
 
-    public static void DistractAll(float distractionValue = 0)
+    public static void DistractAll(float distractionValue)
     {
+        AIJackPlayer[] ais = Object.FindObjectsOfType<AIJackPlayer>();
+        foreach (AIJackPlayer player in ais)
+        {
+            Distract(player, distractionValue);
+        }
     }
 }
