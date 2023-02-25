58ad10003ab6f2e3443682191ea277c90a9ef5be
Tommy Gobeil <tommygob10@hotmail.ca>
2023-02-24 22:55:05 -0500

    SmartPigeCheck réeussi pour vrai?

--- Assets/scripts/AIDecision.cs
+++ Assets/scripts/AIDecision.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIDecision
+public class AIDecision: MonoBehaviour
 {
     //Remplacer par get Card plus tard
     List<Card> ai_cards = new List<Card>();
@@ -15,78 +15,71 @@ public class AIDecision
 
     int score = HandValue(ai_values);
 
-    if (score => 21)
-        {
-        //do nothing
-        }
-    else if (intelligent){
-        if (SmartPigeCheck(score, table_values, dealer_card)){
-        //pige
-        }
-        else
-        {
-        //do nothing
-        }
-    }else
-    {
-        //if (DumbPigeCheck()) pige
-        //else do nothing
-    }
+    
 
        
     
-
-
-
-
-
-public bool SmartPigeCheck(int score, List<int> table_values, Card dealer_card)
-{
-    //Créer le nombre de carte de chaque value (index 0 = nombre de value 2)
-    int[] all_cards = new int[] { 16, 16, 16, 16, 16, 16, 16, 16, 64, 16 };
-    int[] remaining_cards = all_cards;
-    bool pige = false;
-
-    foreach (int value in table_values)
+    public bool Pige() 
     {
-        remaining_cards[value - 2] -= 1;
-    }
-    int nb_usefull_card = 0;
-
-    for (int i = 0; i < 9; i++)
-    {
-        if ((score + i + 2) =< 21)
+        bool pige = false;
+        if (score < 21)
         {
-            nb_usefull_card += remaining_cards[i];
+            if (intelligent)
+            {
+                pige = SmartPigeCheck(score, table_values, dealer_card);
+            }
+            else
+            {
+                //pige = DumbPigeChecl()
+            }
         }
+        return pige;
+            
     }
-    //On ajoute les as parce que si on a pas 21 les as vont toujours nous aider
-    nb_usefull_card += remaining_cards[9];
-
-    int nb_remaining_card = 0;
-
-    for (int i = 0; i < remaining_cards.Length; i++)
+    
+    
+    public bool SmartPigeCheck(int score, List<int> table_values, Card dealer_card)
     {
-        nb_remaining_card += remaining_cards[i];
+        //Créer le nombre de carte de chaque value (index 0 = nombre de value 2)
+        int[] all_cards = new int[] { 16, 16, 16, 16, 16, 16, 16, 16, 64, 16 };
+        int[] remaining_cards = all_cards;
+        bool pige = false;
+        foreach (int value in table_values)
+        {
+            remaining_cards[value - 2] -= 1;
+        }
+        int nb_usefull_card = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            if ((score + i + 2) =< 21)
+            {
+                nb_usefull_card += remaining_cards[i];
+            }
+        }
+        //On ajoute les as parce que si on a pas 21 les as vont toujours nous aider
+        nb_usefull_card += remaining_cards[9];
+        
+        int nb_remaining_card = 0;
+        for (int i = 0; i < remaining_cards.Length; i++)
+        {
+            nb_remaining_card += remaining_cards[i];
+        }
+        float odds_of_usefull_card = nb_usefull_card / nb_remaining_card;
+        if (dealer_card.value => 10){
+            if (odds_of_usefull_card => 0.4)
+            {
+                pige = true;
+            }
+        }
+        else
+        {
+            if (odds_of_usefull_card => 0.5)
+            {
+                pige = true;
+            }
+        }
+        return pige;
     }
-
-    float odds_of_usefull_card = nb_usefull_card / nb_remaining_card;
-
-    if (dealer_card.value => 10){
-    if (odds_of_usefull_card => 0.4)
-    {
-        pige = true;
-    }
-}
-else
-{
-if (odds_of_usefull_card => 0.5)
-    {
-        pige = true;
-    }
-}
-return (pige)
-}
     
 
 
@@ -95,6 +88,7 @@ return (pige)
         
       
       
+
         
         
 
