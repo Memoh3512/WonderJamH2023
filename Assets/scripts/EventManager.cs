ed70afd698a2cc01bec8b83b1b646f7406ee5dc5
Catherine Bergeron <bergeron.catherine@icloud.com>
2023-02-24 20:59:28 -0500

    les modifs à Antoine

--- Assets/scripts/EventManager.cs
+++ Assets/scripts/EventManager.cs
@@ -7,41 +7,29 @@ public class EventManager : MonoBehaviour
     private List<JackEvent> EventList= new List<JackEvent>();
     public float minEventTime = 20;
     public float maxEventTime = 45;
-    private bool firstEvent;
-    
     // Start is called before the first frame update
     void Start()
     {
        //faire un add pour chaque type d'évent
         StartCoroutine(EventRoutine());
-        firstEvent = true;
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+           
     }
 
     IEnumerator EventRoutine()
     {
-
-        while(true)
-        {
-            if (firstEvent)
-            {
-                yield return new WaitForSeconds( Random.Range(minEventTime, maxEventTime));
-                firstEvent = false;
-
-            }
-
-            Debug.Log("Execute event!");
-            if (EventList.Count > 0) EventList[Random.Range(0, EventList.Count - 1)].ExecuteEvent();
-            yield return new WaitForSeconds(Random.Range(minEventTime, maxEventTime));
-
-        }
+        yield return new WaitForSeconds(Random.Range(minEventTime, maxEventTime));
+        JackEvent randomEvent = null;
+        if(randomEvent != null)randomEvent = EventList[Random.Range(0, EventList.Count - 1)];
+        randomEvent.ExecuteEvent();
+        randomEvent.addListenerEventEnded(() => {
+            StartCoroutine(EventRoutine());
+        }); 
+        
         
     }
 
