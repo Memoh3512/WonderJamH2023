ab32cc814dee7f1f84f6706c426f4a8a298df56d
Mattys Gervais <48138387+liperium@users.noreply.github.com>
2023-02-25 09:40:34 -0500

    Hand stays inside box!

--- Assets/scripts/BrasManager.cs
+++ Assets/scripts/BrasManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BrasManager : MonoBehaviour
@@ -12,6 +13,7 @@ public class BrasManager : MonoBehaviour
     public GameObject bras_target;
     public GameObject rotation_base_obj;
     public Transform actual_hand_transform;
+    public PolygonCollider2D bounding_collider;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,21 +27,24 @@ public class BrasManager : MonoBehaviour
     {
         if (canMove)
         {
-            Vector3 pos = camera.ScreenToWorldPoint(Input.mousePosition);
-            pos.z = 0;
-            bras_target_transform.position = pos;
-            
-            
+            Vector3 world_mouse_pos = camera.ScreenToWorldPoint(Input.mousePosition);
+            world_mouse_pos.z = 0;
+
+            float speed = 5f;
+            float step = speed * Time.deltaTime;
+            Vector2 closet_point = bounding_collider.ClosestPoint(world_mouse_pos);
+            bras_target_transform.position = Vector3.Lerp(bras_target_transform.position, closet_point, step);
+
             //TODO rotation fix
-            var position0 = actual_hand_transform.position;
-            var position1 = rotation_base.position;
-            
+            //var position0 = actual_hand_transform.position;
+            //var position1 = rotation_base.position;
+        
             /*bras_target_transform.eulerAngles = new Vector3(0,0,(float)(Mathf.Rad2Deg*Math.Atan2(
                 position0.y-position1.y,
                 Math.Abs(position0.x-position1.x)
                 )));*/
             //bras_target_transform.rotation = Quaternion.LookRotation(bras_target_transform.position - rotation_base.position);
-
+            
         }
     }
 }
