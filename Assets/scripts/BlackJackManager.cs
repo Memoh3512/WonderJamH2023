b704e8e8a00ac5e7957b4cc150774ef9e2a17673
Memoh <CraftMan3512@gmail.com>
2023-02-26 09:07:06 -0500

    game end win quand on a >200 tokens

--- Assets/scripts/BlackJackManager.cs
+++ Assets/scripts/BlackJackManager.cs
@@ -155,6 +155,10 @@ public class BlackJackManager : MonoBehaviour
                 {
                     player.money += 20;
                     player.expressionManager.HappyExpression();
+                    if (player.intel && player.money >= 200)
+                    {
+                        GameEnd(true);
+                    }
                 }
             }
             yield return new WaitForSeconds(6);
